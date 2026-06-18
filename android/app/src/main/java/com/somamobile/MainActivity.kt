fa8a5551b24b5e00d0e07c9bb5b97a105b6ee8bc
Arif Saputra <arifsaputra2026@gmail.com>
2026-06-18 03:31:50 +0000

    feat: add Android native project structure

---
+++ android/app/src/main/java/com/somamobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.somamobile
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+    override fun getMainComponentName(): String = "SomaMobile"
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
